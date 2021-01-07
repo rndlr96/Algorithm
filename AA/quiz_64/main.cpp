0121335907ad1730f1a85121143597e7a88ce98a
rnldr96 <rndlr96>
2021-01-07 15:11:34 +0900

    upload algorithm solution

--- AA/quiz_64/main.cpp
+++ AA/quiz_64/main.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int size, cnt = 0;
 int arr[30][30];
 int check[30];
+
 void DFS(int k){
     int i;
     if( k == size-1 ){
         cnt += 1;
     } else{
-        for(int i = 0 ; i < size ; i++){
+        for(i = 0 ; i < size ; i++){
             if(arr[k][i] == 1 && check[i] == 0){
                 check[i] = 1;
                 DFS(i);
@@ -24,7 +25,6 @@ void DFS(int k){
 int main() {
     int input, start, end;
     cin >> size >> input;
-    int arr[size][size];
     memset(arr, 0, size*size*sizeof(int));
     for(int i = 0 ; i < input ; i++){
         cin >> start >> end;
